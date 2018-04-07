b91ee2a2611eb7cb7a40f1dfbbb8bce717b5838a
augustoalegon <aagg83556@hotmail.com>
2018-04-07 16:57:21 -0500

    First unit test with jest for App.js

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,13 +1,11 @@
 import 'react-native'
-import React from 'react'
-import App from '../App'
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer'
+import React from 'react'
+import App from '../App/App'
 
 it('renders correctly', () => {
   const tree = renderer.create(
     <App />
   )
-  expect(tree).toMatchSnapshot()
 })
